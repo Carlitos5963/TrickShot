e74aca4575b16cb30b5530afbf91fa9645072542
Jose Carlos Gomez <josecarlosgomez69gmail.com>
2018-11-20 19:42:10 -0800

    gitignore added

--- Trick Shot/Assets/Buttons_Scripts.cs
+++ Trick Shot/Assets/Buttons_Scripts.cs
@@ -13,25 +13,22 @@ public class Buttons_Scripts : MonoBehaviour
     public Button loadleve3;
     public Button credits;
 
-    public string Level1;
-    public string Level2;
-    public string Level3;
-
 
     public void LoadLevel1()
     {
+        //Additive used to load screen over main menu scene
         Debug.Log("Attempting to load level 1");
-        SceneManager.LoadScene(Level1);
+        SceneManager.LoadScene("Level1", LoadSceneMode.Additive);
     }
     public void LoadLevel2()
     {
         Debug.Log("Attempting to load level 2");
-        SceneManager.LoadScene(Level2);
+        SceneManager.LoadScene("Level2", LoadSceneMode.Additive);
     }
     public void LoadLevel3()
     {
         Debug.Log("Attempting to load level 3");
-        SceneManager.LoadScene(Level3);
+        SceneManager.LoadScene("Level3", LoadSceneMode.Additive);
     }
 
     public void ExitGame()
