76701eaf9f3f4169d6220d2e81c8481e75afcce4
Jose Carlos Gomez <josecarlosgomez69gmail.com>
2018-11-25 21:08:13 -0800

    Added player scripts to better control the player as well as a bulletController script to give behavior to bullets

---
+++ Trick Shot/Assets/Scripts/BulletController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+    public float speed = 50f;
+    public float velocityX;
+    public float velocityY;
+    public GameObject player;
+    public GameObject barrel;
+    private Vector2 playerVec;
+    private Vector2 barrelVec;
+    private Vector2 newForce;
+    private float bulletAngle;
+
+    private Rigidbody2D rb;
+    
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        playerVec = player.transform.position;
+        barrelVec = barrel.transform.position;
+        bulletAngle = Vector2.Angle(playerVec, barrelVec);
+        //rb.velocity = new Vector2(Mathf.Cos(bulletAngle), Mathf.Sin(bulletAngle)) * speed;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("bulletAngle = " + bulletAngle);
+        rb.position += new Vector2(Mathf.Cos(bulletAngle), Mathf.Sin(bulletAngle)) * speed;
+    }
+}
+//TO-DO
+//In this script, figure out how to get velocity according to angle between vectors
---
+++ Trick Shot/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    public GameObject bullet;
+    public GameObject barrel;
+    Vector2 barrelPosition;
+
+    public float smooth = 4.0f;
+    public float tiltAngle = 80.0f;
+
+    void Update()
+    {
+        // Smoothly tilts a transform towards a target rotation.
+        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+ 
+        Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.SlerpUnclamped(transform.rotation, target, Time.deltaTime * smooth);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
+    }
+
+    public void shoot()
+    {
+        barrelPosition = barrel.transform.position;
+        var copy = Instantiate(bullet, barrelPosition, Quaternion.identity);
+        Destroy(copy, 3.0f);
+
+    }
+}
+
+//TO-DO
+//In this script, figure out how to instantiate bullet at barrels position
